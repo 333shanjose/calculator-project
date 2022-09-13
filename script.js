819a24a015d447c2b81c080dd6ad3dcbc4d34d0e
shan jose <shanjose32@gmail.com>
2022-09-13 11:17:22 +0530

    javascript functions completed

---
+++ script.js
@@ -0,0 +1,63 @@
+function calculator(valv){
+    
+    
+    
+        
+        
+     
+     document.getElementById("screen").value=document.getElementById("screen").value+valv
+     
+     
+     
+    }
+    
+    
+    
+    
+    
+    
+
+function onclear(){
+    document.getElementById("screen").value=""
+}
+
+    
+
+function equelltwo(){
+    
+    
+    
+    
+
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
+
+    
+
+
+    
+
+
+
+    
+    
+    
+
+    
+    
+
+    
+    
+    
+    
+    
+    
+
+       
+
+       
+   
+       
+   
